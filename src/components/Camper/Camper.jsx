b5ee073698868b2b087af499644aafd8fbf3bfd6
mykola-ovchynnik <ovchynnik.mykola@gmail.com>
2024-04-14 01:21:32 +0200

    added camper component

--- src/components/Camper/Camper.jsx
+++ src/components/Camper/Camper.jsx
@@ -1,3 +1,33 @@
 export const Camper = () => {
-  return <></>;
+  return (
+    <li>
+      <img />
+      <div info>
+        <div className="namePrice">
+          <h3>name</h3>
+          <p>price</p>
+          <button>favorite</button>
+        </div>
+        <div className="reviewLocation">
+          <span>
+            <svg>star</svg>
+            rating
+            <p>total reviews</p>
+          </span>
+          <span>
+            <svg>map_pin_svg</svg>
+            location
+          </span>
+        </div>
+        <p>desciption</p>
+        <ul>
+          list of attributes
+          <li>
+            <svg></svg>2 adults
+          </li>
+        </ul>
+        <button>show more button</button>
+      </div>
+    </li>
+  );
 };
