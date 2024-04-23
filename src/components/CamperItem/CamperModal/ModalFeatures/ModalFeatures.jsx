e25eccfe7f2ad774fc893edc59731b8753720527
mykola-ovchynnik <ovchynnik.mykola@gmail.com>
2024-04-23 22:41:17 +0200

    added details to feature

--- src/components/CamperItem/CamperModal/ModalFeatures/ModalFeatures.jsx
+++ src/components/CamperItem/CamperModal/ModalFeatures/ModalFeatures.jsx
@@ -2,7 +2,14 @@ import { useSelector } from 'react-redux';
 import { singleCamperSelector } from '../../../../store/campersReducer/campersSlice';
 import { getAttributes } from '../../../../utils/getAttributes';
 import { CamperAttributes } from '../../../CamperAttributes/CamperAttributes';
-import { FeaturesWrapper } from './ModalFeatures.styled';
+import {
+  FeaturesWrapper,
+  ModalDetails,
+  ModalDetailsItem,
+  ModalDetailsList,
+  ModalDetailsText,
+  ModalDetailsTitle,
+} from './ModalFeatures.styled';
 
 export const ModalFeatures = () => {
   const camper = useSelector(singleCamperSelector);
@@ -11,7 +18,36 @@ export const ModalFeatures = () => {
   return (
     <>
       <FeaturesWrapper>
-        <CamperAttributes attributes={attributes} />;
+        <CamperAttributes attributes={attributes} />
+        <ModalDetails>
+          <ModalDetailsTitle>Vehicle details</ModalDetailsTitle>
+          <ModalDetailsList>
+            <ModalDetailsItem>
+              <ModalDetailsText>Form</ModalDetailsText>
+              <ModalDetailsText>{camper.form}</ModalDetailsText>
+            </ModalDetailsItem>
+            <ModalDetailsItem>
+              <ModalDetailsText>Length</ModalDetailsText>
+              <ModalDetailsText>{camper.length}</ModalDetailsText>
+            </ModalDetailsItem>
+            <ModalDetailsItem>
+              <ModalDetailsText>Width</ModalDetailsText>
+              <ModalDetailsText>{camper.width}</ModalDetailsText>
+            </ModalDetailsItem>
+            <ModalDetailsItem>
+              <ModalDetailsText>Height</ModalDetailsText>
+              <ModalDetailsText>{camper.height}</ModalDetailsText>
+            </ModalDetailsItem>
+            <ModalDetailsItem>
+              <ModalDetailsText>Tank</ModalDetailsText>
+              <ModalDetailsText>{camper.tank}</ModalDetailsText>
+            </ModalDetailsItem>
+            <ModalDetailsItem>
+              <ModalDetailsText>Consumption</ModalDetailsText>
+              <ModalDetailsText>{camper.consumption}</ModalDetailsText>
+            </ModalDetailsItem>
+          </ModalDetailsList>
+        </ModalDetails>
       </FeaturesWrapper>
     </>
   );
--- src/components/CamperItem/CamperModal/ModalFeatures/ModalFeatures.styled.js
+++ src/components/CamperItem/CamperModal/ModalFeatures/ModalFeatures.styled.js
@@ -3,5 +3,48 @@ import styled from 'styled-components';
 export const FeaturesWrapper = styled.div`
   width: 430px;
   display: flex;
+  flex-direction: column;
   gap: 44px;
+`;
+
+export const ModalDetails = styled.div``;
+
+export const ModalDetailsTitle = styled.h3`
+  color: ${({ theme }) => theme.text};
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 24px;
+  position: relative;
+
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: -24px;
+    width: 100%;
+    height: 1px;
+    background-color: ${({ theme }) => theme.border};
+    z-index: 5;
+  }
+`;
+
+export const ModalDetailsList = styled.ul`
+  margin-top: 48px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
+
+export const ModalDetailsItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ModalDetailsText = styled.p`
+  color: ${({ theme }) => theme.text};
+  text-align: center;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 24px;
 `;
